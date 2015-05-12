eb79a357e14a32239a505d7739f5ac156d5b8084
Marcus Nascimento <m.vinicius@samsung.com>
2015-05-12 10:26:26 -0300

    Removing copyrighted Projects info.

--- getFiles.sh
+++ getFiles.sh
@@ -2,6 +2,8 @@
 
 cp ~/SetEnv.sh ./
 cp -r ~/MVNEnv ./
+rm -f ./MVNEnv/Projs/{ICache.h,IMultimedia.sh,Security.sh,Tizen.sh,Default}
+ln -sf Study.sh ./MVNEnv/Projs/Default
 cp -r ~/cscope ./
 rm -rf ./cscope/indexes/*
 cp -r ~/.vim ./Vim
