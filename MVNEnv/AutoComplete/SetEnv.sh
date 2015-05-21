91d36ad144199b284cb1d3227bc57c872eec81fa
Marcus Nascimento <m.vinicius@samsung.com>
2015-05-21 15:57:09 -0300

    Fix typo.

--- MVNEnv/AutoComplete/SetEnv.sh
+++ MVNEnv/AutoComplete/SetEnv.sh
@@ -3,5 +3,5 @@ _SetEnvSh()
 	local cur=${COMP_WORDS[COMP_CWORD]}
 	COMPREPLY=( $(compgen -W "$(ls ${ENV_DIR}/Projs/ | cut -d'.' -f1)" -X Default -- $cur) )
 }
-complete -F _SetEnvSg SetEnv.sh
+complete -F _SetEnvSh SetEnv.sh
 
